0ca04c4a99c002bcac30a003b09f3fba7908d5d8
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-01-30 15:34:53 +0100

    quote session label

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ ipde_utils_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 "$ipde_utils_path/deploy.sh" "$@"
 
 session=$1
-"$ipde_utils_path/build.sh" $session
+"$ipde_utils_path/build.sh" "$session"
 
-trap '"$ipde_utils_path/stop.sh" $session' INT
-"$ipde_utils_path/run.sh" $session
+trap '"$ipde_utils_path/stop.sh" "$session"' INT
+"$ipde_utils_path/run.sh" "$session"
--- testing.sh
+++ testing.sh
@@ -4,7 +4,7 @@ ipde_utils_path=$(cd $(dirname "$BASH_SOURCE") && pwd)
 "$ipde_utils_path/create_test.sh" "$@"
 
 session=$1
-"$ipde_utils_path/build.sh" $session
+"$ipde_utils_path/build.sh" "$session"
 
-trap '"$ipde_utils_path/stop.sh" $session' INT
-"$ipde_utils_path/run.sh" $session
+trap '"$ipde_utils_path/stop.sh" "$session"' INT
+"$ipde_utils_path/run.sh" "$session"
